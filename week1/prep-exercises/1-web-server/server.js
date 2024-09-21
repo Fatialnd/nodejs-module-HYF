22dc5bfa3ca8624d8800b20b56ccca96a498833a
Fatemeh Alinejad <fatemeh.alinejad.dev@gmail.com>
2024-09-21 22:34:14 +0200

    prep-ex nodejs

--- week1/prep-exercises/1-web-server/server.js
+++ week1/prep-exercises/1-web-server/server.js
@@ -2,13 +2,31 @@
  * Exercise 3: Create an HTTP web server
  */
 
-const http = require('http');
+const http = require("http");
+const fs = require("fs").promises;
+const path = require("path");
 
-//create a server
-let server = http.createServer(function (req, res) {
-  // YOUR CODE GOES IN HERE
-	res.write('Hello World!'); // Sends a response back to the client
-	res.end(); // Ends the response
+const mimeTypes = {
+  ".html": "text/html",
+  ".js": "application/javascript",
+  ".css": "text/css",
+};
+
+const server = http.createServer(async (req, res) => {
+  let filePath = path.join(__dirname, req.url === "/" ? "index.html" : req.url);
+  let ext = path.extname(filePath);
+  let contentType = mimeTypes[ext] || "text/plain";
+
+  try {
+    const data = await fs.readFile(filePath);
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(data);
+  } catch (err) {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404: File Not Found");
+  }
 });
 
-server.listen(3000); // The server starts to listen on port 3000
+server.listen(3000, () => {
+  console.log("Server is running at http://localhost:3000");
+});
